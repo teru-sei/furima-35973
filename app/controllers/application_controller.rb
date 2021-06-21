c01686f5e3143cd23ca7fd7a9480b0b8621e7271
teru-sei <hfc.3591@gmail.com>
2021-06-21 15:56:28 +0900

    ログイン、ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
   private
 
@@ -12,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_ka,
+                                      keys: [:nickname, :email, :password, :last_name, :first_name, :last_name_ka,
                                              :first_name_ka, :birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname,           presence: true
   validates :email,              presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password,           presence: true
   validates :last_name,          presence: true
   validates :first_name,         presence: true
   validates :last_name_ka,       presence: true
