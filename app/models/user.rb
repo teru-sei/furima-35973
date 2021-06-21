3c81f818775dd5caccb867342f92868964fca9a0
teru-sei <hfc.3591@gmail.com>
2021-06-21 10:53:19 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,           precence: true
+  validates :email,              precence: true, unique: true
+  validates :encrypted_password, precence: true
+  validates :last_name,          precence: true
+  validates :first_name,         precence: true
+  validates :last_name_ka,       precence: true
+  validates :first_name_ka,      precence: true
+  validates :birthday,           precence: true
 end
