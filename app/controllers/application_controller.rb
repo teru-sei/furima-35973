021b13c21250e0a64b64bb38e8f7d498c0ca1e81
teru-sei <hfc.3591@gmail.com>
2021-06-21 12:29:46 +0900

    ストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configured_parmitted_parameters, if: :devise_controller?
 
 
   private
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configured_parmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_ka, :first_name_ka, :birthday])
   end
 end
