d511c5d8e814be0d73d5703c9768168c6055a910
teru-sei <hfc.3591@gmail.com>
2021-06-21 14:59:47 +0900

    ユーザー新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :configured_parmitted_parameters, if: :devise_controller?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
-  def configured_parmitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_ka, :first_name_ka, :birthday])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_ka,
+                                             :first_name_ka, :birthday])
   end
 end
