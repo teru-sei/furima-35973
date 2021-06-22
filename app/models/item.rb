91c7244d81d5e91eb7ee53c8ab5b31e5566adc7f
teru-sei <hfc.3591@gmail.com>
2021-06-22 21:06:12 +0900

    バリデーション、アソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,22 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_price_id
+    validates :delivery_day_id
+    validates :preference_id
+    validates :item_price
+
+    with_options foreign_key: true do
+      validates :user
+    end
+  end
+
+  belongs_to :user
+  has_one :purchase_history
   has_one_attached :image
 end
