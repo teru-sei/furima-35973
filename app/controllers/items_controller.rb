7c8ab3bcd9aea2eb26b66d7894e4d2cc4d49e07f
teru-sei <hfc.3591@gmail.com>
2021-06-24 10:36:03 +0900

    販売手数料、利益の出力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
