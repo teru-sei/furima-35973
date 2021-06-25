afd2c66224d96cba919474b0009f132f1181e415
teru-sei <hfc.3591@gmail.com>
2021-06-25 16:24:33 +0900

    アクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: :edit
-  before_action :move_to_index, only: :edit
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update] 
 
   def index
     @item = Item.order("created_at DESC")
