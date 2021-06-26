f1b4975f9f34e0404b7d9c90e8e8790fe9cd58ff
teru-sei <hfc.3591@gmail.com>
2021-06-26 13:44:21 +0900

    モデル、テーブル作成

---
+++ db/migrate/20210626043419_create_purchases.rb
@@ -0,0 +1,13 @@
+class CreatePurchases < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchases do |t|
+      t.string  :post_code,     null: false
+      t.integer :prefecture_id, null: false
+      t.string  :city,          null: false
+      t.string  :address,       null: false
+      t.string  :building
+      t.string  :phone_number,  null: false
+      t.timestamps
+    end
+  end
+end
