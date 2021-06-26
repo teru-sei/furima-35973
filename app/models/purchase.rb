d48041949be8c35f323a80cb5c9eec9b1fcd899c
teru-sei <hfc.3591@gmail.com>
2021-06-26 13:53:06 +0900

    バリデーション記述

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,14 @@
 class Purchase < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number
+  end
+
+  belongs to :purchase_history
 end
