8950038de4b9bfb4be174305150a393d7466a5a1
teru-sei <hfc.3591@gmail.com>
2021-06-28 19:06:20 +0900

    バリデーション、エラー文追加

--- app/models/purchase_history_purchase.rb
+++ app/models/purchase_history_purchase.rb
@@ -3,11 +3,12 @@ class PurchaseHistoryPurchase
   attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :token, :user_id
 
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, length: { minimum: 10, message: "is too short" }
+    validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input only number" }
     validates :token
   end
 
